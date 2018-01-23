7fb32debdf65759814127bec9573cced291217b2
Andrey Pervakov <yrkup3@gmail.com>
2018-01-23 20:41:51 +0300

    Fix

--- tests/items.ts
+++ tests/items.ts
@@ -4,8 +4,7 @@ global.THREE = require('three');
 describe('Item', function () {
   describe('#resize()', function() {
     it('should throw Error when the scale is invalid', function() {
-      throw new Error(require('../src/items/floor_item'));
-      var FloorItem = require('../src/items/floor_item').Items.FloorItem;
+      var FloorItem = require('../src/items/floor_item');
       var item = new FloorItem();
       assert.throws(item.resize(1, 1, -1), /scale/, 'unexpected error');
     });
